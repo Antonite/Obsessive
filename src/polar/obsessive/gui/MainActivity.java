e7f7efdfd657932dfdbed5990ef896627b64d6da
Anton <axp9304@g.rit.edu>
2013-10-19 21:18:07 -0400

    added multipanes

--- src/polar/obsessive/gui/MainActivity.java
+++ src/polar/obsessive/gui/MainActivity.java
@@ -1,9 +1,13 @@
 package polar.obsessive.gui;
 
+import polar.obsessive.ArFieldListActivity;
 import polar.obsessive.R;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,13 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void navigatePolarBear(View v){ 
+			Log.i("clicks","You Clicked B1"); 
+			Intent i=new Intent(MainActivity.this, ArFieldListActivity.class);
+			startActivity(i); 
+			System.out.println("asdf"); 
 	}
 
 }

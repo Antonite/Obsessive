aea8368ee5f87b9dbc2d3708dbc9aa7eac9517ad
Richard Laughlin <rwl3564@rit.edu>
2013-10-20 00:15:58 -0400

    BLAH ASOIDKFISDJFISJDF

--- src/polar/obsessive/MainActivity.java
+++ src/polar/obsessive/MainActivity.java
@@ -1,5 +1,14 @@
 package polar.obsessive;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
+
 import polar.obsessive.R;
 import android.os.Bundle;
 import android.app.Activity;
@@ -8,14 +17,29 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.graphics.Bitmap;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 	
 	private NotificationManager notificationManager;
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
+	private UiLifecycleHelper uiHelper;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +52,24 @@ public class MainActivity extends Activity {
 		//setContentView(R.layout.activity_main);
 		notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+		 uiHelper = new UiLifecycleHelper(this, callback);
+		 uiHelper.onCreate(savedInstanceState);
+
+		 LoginButton auth = (LoginButton)findViewById(R.id.login_button);
+		 
+		 try {
+			 Log.e("KeyHash:", "WHAT");
+		        PackageInfo info = getPackageManager().getPackageInfo("polar.obsessive", PackageManager.GET_SIGNATURES);
+		        for (Signature signature : info.signatures) {
+		            MessageDigest md = MessageDigest.getInstance("SHA");
+		            md.update(signature.toByteArray());
+		            Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+		            }
+		    } catch (NameNotFoundException e) {
+
+		    } catch (NoSuchAlgorithmException e) {
+
+		    }
 	}
 	
 	public void createNotification(View v){
@@ -57,10 +99,44 @@ public class MainActivity extends Activity {
 		notificationManager.notify(0,n);
 	}
 	
-	public void navigatePolarBear(View v){ 
-			Log.i("clicks","You Clicked B1"); 
-			Intent i=new Intent(MainActivity.this, ArFieldListActivity.class);
-			startActivity(i); 
-			System.out.println("asdf"); 
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+		
+		Log.e("STATE", state.toString());
+		
+	    if (state.isOpened()) {
+	    	startActivity(new Intent(MainActivity.this, ArFieldListActivity.class)); 
+	    } else if (state.isClosed()) {
+	        Log.i("INFO", "Logged out...");
+	    }
+	}
+	
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    uiHelper.onResume();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
 	}
 }
